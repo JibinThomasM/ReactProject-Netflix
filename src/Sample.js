9c62c02c4b7d7c8c29f3aeaad40d58b07a2fb53c
Jibin Thomas <jibinthomas799@gmail.com>
2021-10-10 15:16:38 +0530

    init

---
+++ src/Sample.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import axios from 'axios'
+import {useState} from 'react'
+function Sample() {
+    const [state,setState]=useState([])
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <button onClick={()=>{
+                axios.get('https://jsonplaceholder.typicode.com/posts').then((response)=>{
+                    console.log(response.data)
+                    setState(response.data)
+                })
+            }}>Click Me</button>
+            {
+                state.map((obj,index)=>{
+                    return(
+                        <div>
+                            <h1>{index+1}</h1>
+                            <h1>{obj.title}</h1>
+                            <h4>{obj.body}</h4>
+                        </div>
+                    )
+                })
+            }
+
+        </div>
+    )
+}
+
+export default Sample
